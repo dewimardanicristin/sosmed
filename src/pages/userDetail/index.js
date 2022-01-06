661120e21fabf69307806fdb73e406f32a9ee5f0
dewimardanic <dewimardanic@gmail.com>
2022-01-06 18:45:59 +0800

    profile

--- src/pages/userDetail/index.js
+++ src/pages/userDetail/index.js
@@ -19,10 +19,17 @@ export default function UserProfile(){
           <div className="container">
             <div id="profileUser" className="p-3">
               <div className="justify-content-between d-flex">
-                <h3>{theUser !== undefined ? theUser.name : null}</h3><span><p>{theUser !== undefined ? theUser.email : null}</p></span>
+                <div className="d-flex align-items-center">
+                  <span><i className="far fa-user fa-2x me-3"></i></span><h3>{theUser !== undefined ? theUser.name : null}</h3>
+                </div>
+                <span className="d-flex"><i class="far fa-envelope align-items-center fw-bold mx-1 mt-1" style={{color:"red"}}></i><p>{theUser !== undefined ? theUser.email : null}</p></span>
               </div>
-              <h6>{theUser !== undefined ? theUser.company.name+", "+theUser.company.catchPhrase+", "+theUser.company.bs : null}</h6>
-              <p>{theUser !== undefined ? theUser.address.street+", "+theUser.address.suite+", "+theUser.address.city+", "+theUser.address.zipcode : null}</p>        
+              <div className="d-flex my-2 mx-3">
+                <i className="far fa-building me-3"></i><h6>{theUser !== undefined ? theUser.company.name+", "+theUser.company.catchPhrase+", "+theUser.company.bs : null}</h6>
+              </div>
+              <div className="d-flex mx-3">
+                <span><i class="fas fa-map-marker-alt me-3" style={{color:"red"}}></i></span><p>{theUser !== undefined ? theUser.address.street+", "+theUser.address.suite+", "+theUser.address.city+", "+theUser.address.zipcode : null}</p>   
+              </div>     
             </div>
             <div className="row">
               {/* <div className="col-lg-4 com-md-3 col-sm-6 d-flex"> */}
